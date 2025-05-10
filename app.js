e29c5030670670994472f3299ee352df986e1e9f
duongfjd <121930616+duongfjd@users.noreply.github.com>
2025-05-10 20:45:30 +0700

    Update app.js sửa API

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 // Khởi tạo Firebase
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_AUTH_DOMAIN",
-    databaseURL: "YOUR_DATABASE_URL",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_STORAGE_BUCKET",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
+    apiKey: "AIzaSyAmNTV2Gn_1ja4XyYeUOTjukktzjKcbRAI",
+    authDomain: "apptruyen-670b1.firebaseapp.com",
+    databaseURL: "https://apptruyen-670b1-default-rtdb.firebaseio.com",
+    projectId: "apptruyen-670b1",
+    storageBucket: "apptruyen-670b1.firebasestorage.app",
+    messagingSenderId: "786140899201",
+    appId: "1:786140899201:web:ddb0a74ba0e8b000caaa80",
+    measurementId: "G-GKRH6H98MR"
 };
 
 // Khởi tạo Firebase
